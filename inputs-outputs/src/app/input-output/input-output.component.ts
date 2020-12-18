3bf689eda86c0e0ce625548b41c8c116e8a12dae
ywyxyxzx <ywyxyx@qq.com>
2020-12-19 01:36:23 +0800

    123

---
+++ inputs-outputs/src/app/input-output/input-output.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-input-output',
+  templateUrl: './input-output.component.html',
+  styleUrls: ['./input-output.component.css']
+})
+export class InputOutputComponent {
+  @Input() item: string;
+  @Output() deleteRequest = new EventEmitter<string>();
+
+  lineThrough = '';
+
+  delete() {
+    console.warn('Child says: emiting item deleteRequest with', this.item);
+    this.deleteRequest.emit(this.item);
+    this.lineThrough = this.lineThrough ? '' : 'line-through';
+  }
+}
