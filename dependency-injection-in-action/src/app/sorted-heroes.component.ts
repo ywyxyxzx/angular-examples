e7cb08430449d023affe2c18086e8b8670ab177a
ywyxyxzx <ywyxyx@qq.com>
2022-06-04 23:25:14 +0800

    dependency-injection-in-action

---
+++ dependency-injection-in-action/src/app/sorted-heroes.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+/////// HeroesBaseComponent /////
+@Component({
+  selector: 'app-unsorted-heroes',
+  template: '<div *ngFor="let hero of heroes">{{hero.name}}</div>',
+  providers: [HeroService]
+})
+export class HeroesBaseComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[] = [];
+
+  ngOnInit() {
+    this.heroes = this.heroService.getAllHeroes();
+    this.afterGetHeroes();
+  }
+
+  // Post-process heroes in derived class override.
+  protected afterGetHeroes() {}
+
+}
+
+/////// SortedHeroesComponent /////
+@Component({
+  selector: 'app-sorted-heroes',
+  template: '<div *ngFor="let hero of heroes">{{hero.name}}</div>',
+  providers: [HeroService]
+})
+export class SortedHeroesComponent extends HeroesBaseComponent {
+  constructor(heroService: HeroService) {
+    super(heroService);
+  }
+
+  protected override afterGetHeroes() {
+    this.heroes = this.heroes.sort((h1, h2) => h1.name < h2.name ? -1 :
+            (h1.name > h2.name ? 1 : 0));
+  }
+}
